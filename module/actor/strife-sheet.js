41ea0f868671208ba4f564b0c900a8bce8554e18
Red McGreg <contactus@redriderx.com>
2021-01-01 03:54:12 -0500

    set up interaction between stife and hero sheets

--- module/actor/strife-sheet.js
+++ module/actor/strife-sheet.js
@@ -3,14 +3,13 @@
  * @extends {ActorSheet}
  */
 export class agonStrifeSheet extends ActorSheet {
-
   /** @override */
   static get defaultOptions() {
     return mergeObject(super.defaultOptions, {
-      classes: ["agon", "sheet", "actor", "actor_strife"],
+      classes: ["agon", "sheet", "actor", "actor__strife"],
       template: "systems/agon/templates/actor/strife-sheet.handlebars",
-      width: 600,
-      height: 600,
+      width: 800,
+      height: 800,
       // tabs: [{ navSelector: ".sheet-tabs", contentSelector: ".sheet-body", initial: "description" }]
     });
   }
@@ -51,66 +50,112 @@ export class agonStrifeSheet extends ActorSheet {
     //   li.slideUp(200, () => this.render(false));
     // });
 
-    // Rollable abilities.
-    html.find('.rollable').click(this._onRoll.bind(this));
+    html
+      .on(
+        "click",
+        ".vault-of-heaven .favor-toggle",
+        this._onFavorToggle.bind(this)
+      )
+      .on("click", ".create-contest", this._createContest.bind(this));
   }
 
   /* -------------------------------------------- */
 
-  /**
-   * Handle creating a new Owned Item for the actor using initial data defined in the HTML dataset
-   * @private
-   * @param {Event} event   The originating click event
-   */
-  _onItemCreate(event) {
-    event.preventDefault();
-    const header = event.currentTarget;
-    // Get the type of item to create.
-    const type = header.dataset.type;
-    // Grab any data associated with this control.
-    const data = duplicate(header.dataset);
-    // Initialize a default name.
-    const name = `New ${type.capitalize()}`;
-    // Prepare the item object.
-    const itemData = {
-      name: name,
-      type: type,
-      data: data
-    };
-    // Remove the type from the dataset since it's in the itemData.type prop.
-    delete itemData.data["type"];
+  // /**
+  //  * Handle creating a new Owned Item for the actor using initial data defined in the HTML dataset
+  //  * @private
+  //  * @param {Event} event   The originating click event
+  //  */
+  // _onItemCreate(event) {
+  //   event.preventDefault();
+  //   const header = event.currentTarget;
+  //   // Get the type of item to create.
+  //   const type = header.dataset.type;
+  //   // Grab any data associated with this control.
+  //   const data = duplicate(header.dataset);
+  //   // Initialize a default name.
+  //   const name = `New ${type.capitalize()}`;
+  //   // Prepare the item object.
+  //   const itemData = {
+  //     name: name,
+  //     type: type,
+  //     data: data
+  //   };
+  //   // Remove the type from the dataset since it's in the itemData.type prop.
+  //   delete itemData.data["type"];
 
-    // Finally, create the item!
-    return this.actor.createOwnedItem(itemData);
+  //   // Finally, create the item!
+  //   return this.actor.createOwnedItem(itemData);
+  // }
+
+  // /**
+  //  * Handle clickable rolls.
+  //  * @param {Event} event   The originating click event
+  //  * @private
+  //  */
+  // _onRoll(event) {
+  //   event.preventDefault();
+  //   const element = event.currentTarget;
+  //   const dataset = element.dataset;
+
+  //   if (dataset.roll) {
+  //     let roll = new Roll(dataset.roll, this.actor.data.data);
+  //     let label = dataset.label ? `Rolling ${dataset.label}` : '';
+  //     // roll.roll().toMessage({
+  //     //   speaker: ChatMessage.getSpeaker({ actor: this.actor }),
+  //     //   content: '<h1>woah man</h1>',
+  //     //   flavor: label
+  //     // });
+  //     roll.evaluate();
+  //     roll.result;
+  //     renderTemplate("systems/agon/templates/chat/strife-roll.html", {}).then(html => {
+  //       ChatMessage.create({content: html});
+  //     })
+  //     // ChatMessage.create({
+  //     //   content: '<h1>The Strife Player Rolls</h1>'
+  //     // });
+  //   }
+  // }
+
+  _createContest(event) {
+    event.preventDefault();
+    let d = new Dialog({
+      title: "Create a Contest",
+      buttons: {
+        roll: {
+          icon: '<i class="fas fa-dice-d20"></i>',
+          label: "Make Strife Roll",
+          callback: this._createStrifeRoll.bind(this),
+        },
+      },
+    });
+
+    renderTemplate("systems/agon/templates/dialog/propose-contest.handlebars", {
+      actor: this.actor,
+    }).then((html) => {
+      d.data.content = html;
+      d.render(true);
+    });
+  }
+
+  _createStrifeRoll(html) {
+    let rollFormula = "1d6";
+    let roll = new Roll(rollFormula);
+    roll.evaluate();
+
+    renderTemplate("systems/agon/templates/chat/strife-roll.handlebars", {
+      target: roll.result,
+    }).then((html) => {
+      ChatMessage.create({ content: html });
+    });
   }
 
   /**
-   * Handle clickable rolls.
+   * Handle Favor point clicks
    * @param {Event} event   The originating click event
    * @private
    */
-  _onRoll(event) {
-    event.preventDefault();
-    const element = event.currentTarget;
-    const dataset = element.dataset;
-
-    if (dataset.roll) {
-      let roll = new Roll(dataset.roll, this.actor.data.data);
-      let label = dataset.label ? `Rolling ${dataset.label}` : '';
-      // roll.roll().toMessage({
-      //   speaker: ChatMessage.getSpeaker({ actor: this.actor }),
-      //   content: '<h1>woah man</h1>',
-      //   flavor: label
-      // });
-      roll.evaluate();
-      roll.result;
-      renderTemplate("systems/agon/templates/chat/strife-roll.html", {}).then(html => {
-        ChatMessage.create({content: html});
-      })
-      // ChatMessage.create({
-      //   content: '<h1>The Strife Player Rolls</h1>'
-      // });
-    }
+  _onFavorToggle(event) {
+    //
   }
-
 }
--- module/agon.js
+++ module/agon.js
@@ -52,7 +52,39 @@ Hooks.once('init', async function() {
   // Handlebars.registerHelper('toLowerCase', function(str) {
   //   return str.toLowerCase();
   // });
+  // Handlebars.registerHelper('svgPath', function(){
+  //   return chunk.toString();
+  // });
+
+  loadTemplates([
+    "systems/agon/assets/vault-of-heaven.svg.handlebars",
+  ]);
 });
 
 // Add listeners for chat card actions
 Hooks.on("renderChatLog", (app, html, data) => agonActor.chatListeners(html));
+
+// /**
+//  * Define a set of template paths to pre-load
+//  * Pre-loaded templates are compiled and cached for fast access when rendering
+//  * @return {Promise}
+//  */
+// export const preloadHandlebarsTemplates = async function() {
+//   return loadTemplates([
+
+//     // Shared Partials
+//     "systems/dnd5e/templates/actors/parts/active-effects.html",
+
+//     // Actor Sheet Partials
+//     "systems/dnd5e/templates/actors/parts/actor-traits.html",
+//     "systems/dnd5e/templates/actors/parts/actor-inventory.html",
+//     "systems/dnd5e/templates/actors/parts/actor-features.html",
+//     "systems/dnd5e/templates/actors/parts/actor-spellbook.html",
+
+//     // Item Sheet Partials
+//     "systems/dnd5e/templates/items/parts/item-action.html",
+//     "systems/dnd5e/templates/items/parts/item-activation.html",
+//     "systems/dnd5e/templates/items/parts/item-description.html",
+//     "systems/dnd5e/templates/items/parts/item-mountable.html"
+//   ]);
+// };
