8d49a584efa4af163c60cce41c12c59d59988b03
Red McGreg <contactus@redriderx.com>
2021-12-06 01:54:53 -0500

    get fate line working

--- module/actor/hero-sheet.js
+++ module/actor/hero-sheet.js
@@ -19,18 +19,51 @@ export class agonHeroSheet extends ActorSheet {
 
   /** @override */
   getData() {
-    const data = super.getData();
-    const actorData = data.actor.data;
-    data.config = CONFIG.AGON;
+    const sheetData = super.getData();
+    const actorData = sheetData.actor.data;
+    sheetData.config = CONFIG.AGON;
     
     // Re-define the template data references (backwards compatible)
-    data.item = actorData;
-    data.data = actorData.data;
-    data.pathosChecked = [];
-    for (let index = actorData.data.pathos.min; index < actorData.data.pathos.max; index++) {
-      data.pathosChecked[index] = actorData.data.pathos.value === index;
+    sheetData.item = actorData;
+    sheetData.data = actorData.data;
+
+    sheetData.pathosChecked = [];
+    for (let index = actorData.data.pathos.min; index <= actorData.data.pathos.max; index++) {
+      sheetData.pathosChecked[index] = actorData.data.pathos.value == index;
     }
-    return data;
+
+    sheetData.fateChecked = [];
+    for (let index = actorData.data.fate.min; index <= actorData.data.fate.max; index++) {
+      sheetData.fateChecked[index] = actorData.data.fate.value >= index;
+    }
+    
+    return sheetData;
+  }
+
+  /** @override */
+  activateListeners(html) {
+    super.activateListeners(html);
+
+    // Everything below here is only needed if the sheet is editable
+    if (!this.options.editable) return;
+
+    html
+      .on(
+        "click",
+        ".fate-line [type='checkbox']",
+        this._updateFate.bind(this)
+      );
+  }
+
+  async _updateFate(e) {
+    let newValue = e.target.value;
+    return this.actor.update({
+      'data': {
+        'fate': {
+          'value': newValue
+        }
+      }
+    });
   }
 
   /* -------------------------------------------- */
