95ec959ab9e6126e3c61b3fa5d3de3665e0ba9a2
Red McGreg <contactus@redriderx.com>
2021-12-06 17:10:50 -0500

    get scalling bonds and trophies working

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 `npm run start` to build css with postcss and tailwind
+
+---
+notes
+button for strife player to call contest
+roll all gathered names
+    search each log for ref to contest message, stop at next contest roll
+have public chat summarizing suffers, prevails, and best?
+have whispers to each hero with prompts to Recite Deeds
+
+game.messages.filter(message => message.getFlag('agon', 'relatedContest') === <id> )
+game.messages.get(<id>)
+message.setFlag('agon', 'relatedContest', <id>)
--- module/actor/hero-sheet.js
+++ module/actor/hero-sheet.js
@@ -3,7 +3,6 @@
  * @extends {ActorSheet}
  */
 export class agonHeroSheet extends ActorSheet {
-
   /** @override */
   static get defaultOptions() {
     return mergeObject(super.defaultOptions, {
@@ -22,21 +21,36 @@ export class agonHeroSheet extends ActorSheet {
     const sheetData = super.getData();
     const actorData = sheetData.actor.data;
     sheetData.config = CONFIG.AGON;
-    
+
     // Re-define the template data references (backwards compatible)
     sheetData.item = actorData;
     sheetData.data = actorData.data;
 
     sheetData.pathosChecked = [];
-    for (let index = actorData.data.pathos.min; index <= actorData.data.pathos.max; index++) {
+    for (
+      let index = actorData.data.pathos.min;
+      index <= actorData.data.pathos.max;
+      index++
+    ) {
       sheetData.pathosChecked[index] = actorData.data.pathos.value == index;
     }
 
     sheetData.fateChecked = [];
-    for (let index = actorData.data.fate.min; index <= actorData.data.fate.max; index++) {
+    for (
+      let index = actorData.data.fate.min;
+      index <= actorData.data.fate.max;
+      index++
+    ) {
       sheetData.fateChecked[index] = actorData.data.fate.value >= index;
     }
-    
+
+    if (actorData.data.bonds[actorData.data.bonds.length-1].name !== "") {
+      actorData.data.bonds.push({ name: "", strength: 0 });
+    }
+    if (actorData.data.trophies[actorData.data.trophies.length-1].name !== "") {
+      actorData.data.trophies.push({ name: "", strength: 0 });
+    }
+
     return sheetData;
   }
 
@@ -47,23 +61,87 @@ export class agonHeroSheet extends ActorSheet {
     // Everything below here is only needed if the sheet is editable
     if (!this.options.editable) return;
 
-    html
-      .on(
-        "click",
-        ".fate-line [type='checkbox']",
-        this._updateFate.bind(this)
-      );
+    html.on(
+      "click",
+      ".fate-line [type='checkbox']",
+      this._updateFate.bind(this)
+    ).on(
+      "change",
+      ".bond input",
+      this._onBondChange.bind(this)
+    ).on(
+      "change",
+      ".trophy input",
+      this._onTrophyChange.bind(this)
+    );
   }
 
   async _updateFate(e) {
     let newValue = e.target.value;
     return this.actor.update({
-      'data': {
-        'fate': {
-          'value': newValue
-        }
-      }
+      data: {
+        fate: {
+          value: newValue,
+        },
+      },
     });
+  }
+
+  /**
+   * Handle saving a bond in-sheet.
+   * @param {Event} e  Triggering event.
+   * @returns {Promise<Actor5e>|null}  Actor after update if any changes were made.
+   * @private
+   */
+   _onBondChange(e) {
+    e.preventDefault();
+    const target = e.currentTarget;
+    const row = target.closest(".bond");
+    const bondId = Number(row.dataset.id);
+
+    // Get the entry
+    const bonds = foundry.utils.deepClone(this.actor.data.data.bonds);
+    const bond = bonds[bondId];
+    if (!bond) return null;
+
+    // Update the value
+    const key = target.dataset.property || "name";
+    const type = target.dataset.dtype;
+    let value = target.value;
+    if (type === "Number") value = Number(value);
+    bond[key] = value;
+
+    // Perform the Actor update
+    return this.actor.update({'data.bonds': bonds});
+  }
+  
+  /**
+   * Handle saving a trophy in-sheet.
+   * @param {Event} e  Triggering event.
+   * @returns {Promise<Actor5e>|null}  Actor after update if any changes were made.
+   * @private
+   */
+   _onTrophyChange(e) {
+    e.preventDefault();
+    const target = e.currentTarget;
+    const row = target.closest(".trophy");
+    const trophyId = Number(row.dataset.id);
+
+    // Get the entry
+    const trophies = foundry.utils.deepClone(this.actor.data.data.trophies);
+    const trophy = trophies[trophyId];
+    if (!trophy) return null;
+
+    // Update the value
+    const key = target.dataset.property || "name";
+    const type = target.dataset.dtype;
+    let value = target.value;
+    if (type === "Number") value = Number(value);
+    if (type === "Boolean") value = target.checked;
+    trophy[key] = value;
+
+    // Perform the Actor update
+    return this.actor.update({'data.trophies': trophies});
   }
 
   /* -------------------------------------------- */
