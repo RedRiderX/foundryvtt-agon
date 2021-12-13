097bec0ffd1b9b0ac38d469cab110fb23d3c624b
Red McGreg <contactus@redriderx.com>
2021-12-13 15:37:24 -0500

    finish vault stars

--- module/actor/strife-sheet.js
+++ module/actor/strife-sheet.js
@@ -15,11 +15,6 @@ export class agonStrifeSheet extends ActorSheet {
 
   /* -------------------------------------------- */
   starsLayout = {
-    zeus: {
-      one: { top: 1, left: 1 },
-      two: { top: 10, left: 10 },
-      three: { top: 20, left: 20 },
-    },
     aphrodite: {
       one: { top: 19.8, left: 7.5 },
       two: {
@@ -31,55 +26,60 @@ export class agonStrifeSheet extends ActorSheet {
         left: 7.5,
       },
     },
-    demeter: {
-      one: { top: 1, left: 1 },
-      two: { top: 10, left: 10 },
-      three: { top: 20, left: 20 },
-    },
-    hera: {
-      one: { top: 1, left: 1 },
-      two: { top: 10, left: 10 },
-      three: { top: 20, left: 20 },
-    },
-    hermes: {
-      one: { top: 1, left: 1 },
-      two: { top: 10, left: 10 },
-      three: { top: 20, left: 20 },
+    apollo: {
+      one: { top: 47.5, left: 1.8 },
+      two: { top: 47.5, left: 7.4 },
+      three: { top: 47.5, left: 13.1 },
     },
     ares: {
       one: { top: 7.5, left: 28.5 },
       two: { top: 12.3, left: 24.2 },
       three: { top: 16.9, left: 20.1 },
     },
-    poseidon: {
-      one: { top: 1, left: 1 },
-      two: { top: 10, left: 10 },
-      three: { top: 20, left: 20 },
-    },
-    hephaistos: {
-      one: { top: 1, left: 1 },
-      two: { top: 12.3, left: 24.2 },
-      three: { top: 20, left: 20 },
-    },
-    hekate: {
-      one: { top: 1, left: 1 },
-      two: { top: 10, left: 10 },
-      three: { top: 20, left: 20 },
-    },
-    apollo: {
-      one: { top: 47.5, left: 1.8 },
-      two: { top: 47.5, left: 7.4 },
-      three: { top: 47.5, left: 13.1 },
-    },
     artemis: {
-      one: { top: 1, left: 1 },
-      two: { top: 10, left: 10 },
-      three: { top: 20, left: 20 },
+      one: { top: 37.8, left: 18.8 },
+      two: { top: 37.8, left: 24.2 },
+      three: { top: 37.8, left: 29 },
     },
     athena: {
-      one: { top: 1, left: 1 },
-      two: { top: 10, left: 10 },
-      three: { top: 20, left: 20 },
+      one: { top: 20.7, left: 33.9 },
+      two: { top: 20.7, left: 40.2 },
+      three: { top: 20.7, left: 46.9 },
+    },
+    demeter: {
+      one: { top: 39.8, left: 37.3 },
+      two: { top: 52.7, left: 43.6 },
+      three: { top: 56, left: 46.4 },
+    },
+    hekate: {
+      one: { top: 2.5, left: 57.1 },
+      two: { top: 11.6, left: 57.1 },
+      three: { top: 19.5, left: 57.1 },
+    },
+    hephaistos: {
+      one: { top: 31.8, left: 56.9 },
+      two: { top: 37.5, left: 56.9 },
+      three: { top: 45.5, left: 56.9 },
+    },
+    hera: {
+      one: { top: 20.1, left: 76 },
+      two: { top: 24.8, left: 71.8 },
+      three: { top: 29.6, left: 67.5 },
+    },
+    hermes: {
+      one: { top: 40.1, left: 73.5 },
+      two: { top: 47.5, left: 67.5 },
+      three: { top: 47.5, left: 79 },
+    },
+    poseidon: {
+      one: { top: 2.4, left: 87.6 },
+      two: { top: 13.6, left: 87.6 },
+      three: { top: 22.2, left: 87.6 },
+    },
+    zeus: {
+      one: { top: 39.6, left: 85.3 },
+      two: { top: 39.6, left: 90 },
+      three: { top: 39.6, left: 94.9 },
     },
   };
 
