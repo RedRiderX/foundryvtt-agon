0a376e7c038061568f7f62d9bfd9fb85b23cb3ec
Red McGreg <contactus@redriderx.com>
2021-12-13 17:59:27 -0500

    finish wrath checkboxes

--- module/actor/strife-sheet.js
+++ module/actor/strife-sheet.js
@@ -1,3 +1,6 @@
+import starsLayout from "../starsLayout.js";
+import wrathLayout from "../wrathLayout.js";
+
 /**
  * Extend the basic ActorSheet with some very simple modifications
  * @extends {ActorSheet}
@@ -14,74 +17,6 @@ export class agonStrifeSheet extends ActorSheet {
   }
 
   /* -------------------------------------------- */
-  starsLayout = {
-    aphrodite: {
-      one: { top: 19.8, left: 7.5 },
-      two: {
-        top: 26.5,
-        left: 7.5,
-      },
-      three: {
-        top: 33.4,
-        left: 7.5,
-      },
-    },
-    apollo: {
-      one: { top: 47.5, left: 1.8 },
-      two: { top: 47.5, left: 7.4 },
-      three: { top: 47.5, left: 13.1 },
-    },
-    ares: {
-      one: { top: 7.5, left: 28.5 },
-      two: { top: 12.3, left: 24.2 },
-      three: { top: 16.9, left: 20.1 },
-    },
-    artemis: {
-      one: { top: 37.8, left: 18.8 },
-      two: { top: 37.8, left: 24.2 },
-      three: { top: 37.8, left: 29 },
-    },
-    athena: {
-      one: { top: 20.7, left: 33.9 },
-      two: { top: 20.7, left: 40.2 },
-      three: { top: 20.7, left: 46.9 },
-    },
-    demeter: {
-      one: { top: 39.8, left: 37.3 },
-      two: { top: 52.7, left: 43.6 },
-      three: { top: 56, left: 46.4 },
-    },
-    hekate: {
-      one: { top: 2.5, left: 57.1 },
-      two: { top: 11.6, left: 57.1 },
-      three: { top: 19.5, left: 57.1 },
-    },
-    hephaistos: {
-      one: { top: 31.8, left: 56.9 },
-      two: { top: 37.5, left: 56.9 },
-      three: { top: 45.5, left: 56.9 },
-    },
-    hera: {
-      one: { top: 20.1, left: 76 },
-      two: { top: 24.8, left: 71.8 },
-      three: { top: 29.6, left: 67.5 },
-    },
-    hermes: {
-      one: { top: 40.1, left: 73.5 },
-      two: { top: 47.5, left: 67.5 },
-      three: { top: 47.5, left: 79 },
-    },
-    poseidon: {
-      one: { top: 2.4, left: 87.6 },
-      two: { top: 13.6, left: 87.6 },
-      three: { top: 22.2, left: 87.6 },
-    },
-    zeus: {
-      one: { top: 39.6, left: 85.3 },
-      two: { top: 39.6, left: 90 },
-      three: { top: 39.6, left: 94.9 },
-    },
-  };
 
   /** @override */
   getData() {
@@ -96,9 +31,23 @@ export class agonStrifeSheet extends ActorSheet {
 
       Object.keys(actorData.data.vault.stars[godKey]).forEach((starIndex) => {
         sheetData.starsLayout[godKey][starIndex] = {
-          top: this.starsLayout[godKey][starIndex].top,
-          left: this.starsLayout[godKey][starIndex].left,
+          top: starsLayout[godKey][starIndex].top,
+          left: starsLayout[godKey][starIndex].left,
           checked: actorData.data.vault.stars[godKey][starIndex],
+        };
+      });
+    });
+
+    sheetData.wrathLayout = {};
+    Object.keys(actorData.data.vault.wrath).forEach((godKey) => {
+      if (sheetData.wrathLayout[godKey] == null)
+        sheetData.wrathLayout[godKey] = {};
+
+      Object.keys(actorData.data.vault.wrath[godKey]).forEach((wrathIndex) => {
+        sheetData.wrathLayout[godKey][wrathIndex] = {
+          top: wrathLayout[godKey][wrathIndex].top,
+          left: wrathLayout[godKey][wrathIndex].left,
+          checked: actorData.data.vault.wrath[godKey][wrathIndex],
         };
       });
     });
---
+++ module/wrathLayout.js
@@ -0,0 +1,62 @@
+export default {
+  aphrodite: {
+    one: { top: 83.7, left: 2.5 },
+    two: { top: 83.7, left: 7.2 },
+    three: { top: 83.7, left: 11.8 },
+  },
+  apollo: {
+    one: { top: 93.2, left: 2.5 },
+    two: { top: 93.2, left: 7.2 },
+    three: { top: 93.2, left: 11.8 },
+  },
+  ares: {
+    one: { top: 83.7, left: 19 },
+    two: { top: 83.7, left: 23.6 },
+    three: { top: 83.7, left: 28.2 },
+  },
+  artemis: {
+    one: { top: 93.2, left: 19 },
+    two: { top: 93.2, left: 23.6 },
+    three: { top: 93.2, left: 28.2 },
+  },
+  athena: {
+    one: { top: 83.7, left: 35.5 },
+    two: { top: 83.7, left: 40.2 },
+    three: { top: 83.7, left: 44.7 },
+  },
+  demeter: {
+    one: { top: 93.2, left: 35.5 },
+    two: { top: 93.2, left: 40.2 },
+    three: { top: 93.2, left: 44.7 },
+  },
+  hekate: {
+    one: { top: 83.7, left: 51.9 },
+    two: { top: 83.7, left: 56.5 },
+    three: { top: 83.7, left: 61.1 },
+  },
+  hephaistos: {
+    one: { top: 93.2, left: 51.9 },
+    two: { top: 93.2, left: 56.5 },
+    three: { top: 93.2, left: 61.1 },
+  },
+  hera: {
+    one: { top: 83.7, left: 68.3 },
+    two: { top: 83.7, left: 72.9 },
+    three: { top: 83.7, left: 77.4 },
+  },
+  hermes: {
+    one: { top: 93.2, left: 68.3 },
+    two: { top: 93.2, left: 72.9 },
+    three: { top: 93.2, left: 77.4 },
+  },
+  poseidon: {
+    one: { top: 83.7, left: 84.8 },
+    two: { top: 83.7, left: 89.4 },
+    three: { top: 83.7, left: 94 },
+  },
+  zeus: {
+    one: { top: 93.2, left: 84.8 },
+    two: { top: 93.2, left: 89.4 },
+    three: { top: 93.2, left: 94 },
+  },
+};
